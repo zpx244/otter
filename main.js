43417a4a60246ea6488e00e36ca52343715f67a0
zpx244 <124104639@umail.ucc.ie>
2025-08-07 21:21:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import * as THREE from './libs/three.module.js';
-import { ARButton } from './libs/ARButton.js';
+import * as THREE from 'https://unpkg.com/three@0.166.0/build/three.module.js';
+import { ARButton } from 'https://unpkg.com/three@0.166.0/examples/jsm/webxr/ARButton.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.166.0/examples/jsm/loaders/GLTFLoader.js';
 
 let camera, scene, renderer, controller, reticle, cube;
 let hitTestSource = null;
